cf22c2d66cce96ad46849dbb69d467c3902149c9
BrettBeare <beare.logan@yahoo.com>
2025-07-11 08:54:52 -0600

    Project 12, first fix after code review

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
 const itemRoutes = require("./clothingItems");
+const { NOT_FOUND } = require("../utils/errors");
 
 router.use("/users", userRoutes);
 router.use("/items", itemRoutes);
 
 router.use("*", (req, res) => {
-  res.status(404).send({ message: "Requested resource not found" });
+  res.status(NOT_FOUND).send({ message: "Requested resource not found" });
 });
 
 module.exports = router;
